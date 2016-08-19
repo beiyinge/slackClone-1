99cccaae11893789d5f09e16d95127931a1c9c12
redhiker <LarryIrvine@verizon.net>
2016-08-18 21:24:46 -0400

    admin page changes

--- webapp/administration-ctrl.js
+++ webapp/administration-ctrl.js
@@ -3,8 +3,25 @@ slackApp.controller('AdministrationCtrl', function ($scope, $http) {
     $http.get('/allusers').success(function(data) {
         $scope.availableUsers = data;
     });
+    
+    $scope.selectUserToAdd = function() {
+        alert('Selected User:'+$scope.userToAdd);
+
+        $http.get('/team/team/').success(function(data) {
+           $scope.availableTeams = data;
+        });
+    };
+
+    $scope.selectTeamToAdd = function() {
+        alert('Selected Team:'+$scope.teamToAdd.teamName);
+    };
+
 
     $scope.addUserToTeam = function() {
         alert('Selected Name:'+$scope.userToAdd);
+
+       //$http.get('/team/team/').success(function(data) {
+          //$scope.availableTeams = data;
+       //});
     };
 });
