83b858bb8547b538062c24d7033d4a1c139dd3cd
redhiker <LarryIrvine@verizon.net>
2016-08-19 13:42:09 -0400

    changes to admin

--- webapp/administration-ctrl.js
+++ webapp/administration-ctrl.js
@@ -5,6 +5,8 @@ slackApp.controller('AdministrationCtrl', function ($scope, $http) {
     $scope.selectedUserId = -1;
     $scope.selectedTeamId = -1;
 
+    // Get all users.  Get all teams.
+
     $http.get('/allusers')
     .then(function(response) {
         $scope.availableUsers = response.data;
@@ -16,17 +18,23 @@ slackApp.controller('AdministrationCtrl', function ($scope, $http) {
     
     $scope.selectUserToAdd = function(selectedUser) {
 
-        $scope.selectedUserId = selectedUser.id;
+        $scope.selectedUserId = selectedUser.id; // ID of selected user....
+
+        // Get all teams of which the selected user is already a member.
 
         $http.get('/currentTeams/'+selectedUser.id)
         .then(function(response) {
 
            $scope.currentTeams = response.data;
+
            var currentTeamNames = $scope.currentTeams.map(function (team) {
                return team.team;
            });
 
+           // Get all teams of which the selected user is NOT already a member.
+
            $scope.availableTeams = $scope.allTeams.filter(function(team, idx, arr) {
+
                if (currentTeamNames.indexOf(team.teamName) === -1) {
                    return true;
                }
@@ -36,11 +44,20 @@ slackApp.controller('AdministrationCtrl', function ($scope, $http) {
     };
 
     $scope.selectTeamToAdd = function(selectedTeam) {
-        $scope.selectedTeamId = selectedTeam.teamId;
+
+        $scope.selectedTeamId = selectedTeam.teamId; // ID of selected team
     };
 
-    $scope.addUserToTeam = function() {
-        //alert('Adding user:'+$scope.selectedUserId);
-        //alert('Adding user:'+$scope.selectedTeamId);       
+    $scope.addUserToTeam = function() { 
+
+        if ( ($scope.selectedUserId != -1) && ($scope.selectedTeamId != -1) ) {
+
+            var teamUsersDbRow = {"teamId": $scope.selectedTeamId, "userId": $scope.selectedUserId};
+
+            $http.post('/team/user/', teamUsersDbRow).then(function(response) {
+                
+            });
+
+        }   
     };
 });
