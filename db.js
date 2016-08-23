bea653749ac3f22e6d13f90c1204ddbcb42ed410
chedva <chedvaberdy@gmail.com>
2016-08-23 10:15:43 -0400

    db.js syntax fix

--- db.js
+++ db.js
@@ -74,7 +74,7 @@ function createDB(filename) {
             db.run(createTeamUsersTableSql);
             db.run(createChannelsTableSql);
             db.run(createMessageTableSql);
-            db.run(creatChatSql);
+            db.run(createChatSql);
 
 
         });
