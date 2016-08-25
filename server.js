6c58c0b5ddaa4cf9d678e751b46de7e9ff0e2148
zedongzhang <zedongzhang@yahoo.com>
2016-08-25 10:34:55 -0400

    update

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get("/avatar/:userName", function (req, res) {
 	getIntUserIdByUsername(req.params.userName, function(err, data) {
 		//console.log(data);
 		var userId = data;
-		console.log(userId);
+		//console.log(userId);
 		var file = __dirname + '/webapp/avatar/' + userId % 3 + '.jpg';
 		fs.exists(file, function(exists){
       		if (exists) {     
